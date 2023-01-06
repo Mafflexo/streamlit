4de7da06998075d3cb381c5896fce840b52d1b4a
matthieu meunier <matthieu.meunierpro@gmail.com>
2023-01-06 10:11:13 +0100

    first try

---
+++ streamlit.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import requests
+
+def search(keyword, category):
+    base_url = "https://swapi.dev/api/"
+    url = f"{base_url}{category}/?search={keyword}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        results = response.json()['results']
+        return results
+    else:
+        return []
+
+def display_results(results):
+for result in results:
+    name = result['name']
+    st.write(f"Nom : {name}")
+    for key, value in result.items():
+        if key != "name":
+            st.write(f"{key.capitalize()} : {value}")
+    st.write("---")
